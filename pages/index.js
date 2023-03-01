43c6766feea25dbcba46ccc927d3e386997dad53
degenarrative <98234022+degenarrative@users.noreply.github.com>
2023-02-28 16:29:36 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -41,7 +41,6 @@ export default function Home() {
           birthLocation={birthLocation}
           onFormSubmit={handleFormSubmit}
         />
-        <JokeBlock />
       </main>
       <Footer />
     </div>
